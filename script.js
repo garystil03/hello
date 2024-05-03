7d412a4fa61051940fc36a0a487a45a78ec1d053
garystil03 <161110593+garystil03@users.noreply.github.com>
2024-05-03 14:31:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs
 // Function to initialize PDF.js and load the PDF file
 function loadPdf(url) {
   const pdfContainer = document.getElementById('pdf-container');
+  const loadingIndicator = document.getElementById('loading-indicator');
 
   // Function to load PDF content
   function loadPdfContent(pdfDoc) {
@@ -32,6 +33,8 @@ function loadPdf(url) {
       };
       page.render(renderContext).promise.then(function() {
         console.log('Page rendered');
+        // Hide loading indicator once rendering is complete
+        loadingIndicator.style.display = 'none';
       }).catch(function(error) {
         console.error('Error rendering page:', error);
       });
@@ -43,6 +46,9 @@ function loadPdf(url) {
     });
   }
 
+  // Show loading indicator
+  loadingIndicator.style.display = 'block';
+
   // For other browsers, use PDF.js
   pdfjsLib.getDocument(url).promise
   .then(function(pdfDoc) {
@@ -53,6 +59,8 @@ function loadPdf(url) {
   })
   .catch(function(error) {
     console.error('Error loading PDF:', error);
+    // Hide loading indicator on error
+    loadingIndicator.style.display = 'none';
   });
 }
 
