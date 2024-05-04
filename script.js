3b5daff01025d94c48edc04f7d0f8f1b75f4f8e3
garystilAstro <161110593+garystilAstro@users.noreply.github.com>
2024-05-04 12:16:04 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,188 @@
+// Row 1
+var row1 = table.insertRow();
+var cell1 = row1.insertCell();
+cell1.textContent = "Row 1 text";
+
+// Row 2 (3 columns) - Roboto Light Italic
+var row2 = table.insertRow();
+var cell2_1 = row2.insertCell();
+var cell2_2 = row2.insertCell();
+var cell2_3 = row2.insertCell();
+cell2_1.textContent = "Row 2, Col 1 text";
+cell2_2.textContent = "Row 2, Col 2 text";
+cell2_3.textContent = "Row 2, Col 3 text";
+row2.style.fontFamily = "Roboto";
+row2.style.fontStyle = "italic";
+
+// Row 3 (3 columns) - Roboto Light
+var row3 = table.insertRow();
+var cell3_1 = row3.insertCell();
+var cell3_2 = row3.insertCell();
+var cell3_3 = row3.insertCell();
+cell3_1.textContent = "Row 3, Col 1 text";
+cell3_2.textContent = "Row 3, Col 2 text";
+cell3_3.textContent = "Row 3, Col 3 text";
+
+// Row 4
+var row4 = table.insertRow();
+var cell4 = row4.insertCell();
+cell4.textContent = "Row 4 text";
+
+// Row 5
+var row5 = table.insertRow();
+var cell5 = row5.insertCell();
+cell5.textContent = "Row 5 text";
+
+// Row 6 - Image
+var row6 = table.insertRow();
+var cell6 = row6.insertCell();
+var img6 = document.createElement("img");
+img6.src = "p1.png";
+cell6.appendChild(img6);
+
+// Row 7 - Roboto Light Italic
+var row7 = table.insertRow();
+var cell7 = row7.insertCell();
+cell7.textContent = "Row 7 text";
+row7.style.fontFamily = "Roboto";
+row7.style.fontStyle = "italic";
+
+// Row 8
+var row8 = table.insertRow();
+var cell8 = row8.insertCell();
+cell8.textContent = "Row 8 text";
+
+// Row 9
+var row9 = table.insertRow();
+var cell9 = row9.insertCell();
+cell9.textContent = "Row 9 text";
+
+// Row 10 - Image - Roboto Light Italic
+var row10 = table.insertRow();
+var cell10 = row10.insertCell();
+var img10 = document.createElement("img");
+img10.src = "p2.png";
+cell10.appendChild(img10);
+row10.style.fontFamily = "Roboto";
+row10.style.fontStyle = "italic";
+
+// Row 11
+var row11 = table.insertRow();
+var cell11 = row11.insertCell();
+cell11.textContent = "Row 11 text";
+
+// Row 12
+var row12 = table.insertRow();
+var cell12 = row12.insertCell();
+cell12.textContent = "Row 12 text";
+
+// Row 13 - Image - Roboto Light Italic
+var row13 = table.insertRow();
+var cell13 = row13.insertCell();
+var img13 = document.createElement("img");
+img13.src = "p3.png";
+cell13.appendChild(img13);
+row13.style.fontFamily = "Roboto";
+row13.style.fontStyle = "italic";
+
+// Row 14
+var row14 = table.insertRow();
+var cell14 = row14.insertCell();
+cell14.textContent = "Row 14 text";
+
+// Row 15
+var row15 = table.insertRow();
+var cell15 = row15.insertCell();
+cell15.textContent = "Row 15 text";
+
+// Row 16 - Image - Roboto Light Italic
+var row16 = table.insertRow();
+var cell16 = row16.insertCell();
+var img16 = document.createElement("img");
+img16.src = "p4.png";
+cell16.appendChild(img16);
+row16.style.fontFamily = "Roboto";
+row16.style.fontStyle = "italic";
+
+// Row 17
+var row17 = table.insertRow();
+var cell17 = row17.insertCell();
+cell17.textContent = "Row 17 text";
+
+// Row 18
+var row18 = table.insertRow();
+var cell18 = row18.insertCell();
+cell18.textContent = "Row 18 text";
+
+// Row 19 - Image - Roboto Light Italic
+var row19 = table.insertRow();
+var cell19 = row19.insertCell();
+var img19 = document.createElement("img");
+img19.src = "p9.png";
+cell19.appendChild(img19);
+row19.style.fontFamily = "Roboto";
+row19.style.fontStyle = "italic";
+
+// Row 20
+var row20 = table.insertRow();
+var cell20 = row20.insertCell();
+cell20.textContent = "Row 20 text";
+
+// Row 21 (3 columns) - Roboto Light
+var row21 = table.insertRow();
+var cell21_1 = row21.insertCell();
+var cell21_2 = row21.insertCell();
+var cell21_3 = row21.insertCell();
+cell21_1.textContent = "Row 21, Col 1 text";
+cell21_2.textContent = "Row 21, Col 2 text";
+cell21_3.textContent = "Row 21, Col 3 text";
+
+// Row 22
+var row22 = table.insertRow();
+var cell22 = row22.insertCell();
+cell22.textContent = "Row 22 text";
+
+// Row 23 (7 columns) - Roboto Light
+var row23 = table.insertRow();
+for (let i = 1; i <= 7; i++) {
+    var cell23 = row23.insertCell();
+    cell23.textContent = `Row 23, Col ${i} text`;
+}
+
+// Row 24
+var row24 = table.insertRow();
+var cell24 = row24.insertCell();
+cell24.textContent = "Row 24 text";
+
+// Row 25 (3 columns) - Roboto Light
+var row25 = table.insertRow();
+var cell25_1 = row25.insertCell();
+var cell25_2 = row25.insertCell();
+var cell25_3 = row25.insertCell();
+cell25_1.textContent = "Row 25, Col 1 text";
+cell25_2.textContent = "Row 25, Col 2 text";
+cell25_3.textContent = "Row 25, Col 3 text";
+
+// Row 26
+var row26 = table.insertRow();
+var cell26 = row26.insertCell();
+cell26.textContent = "Row 26 text";
+
+// Row 27
+var row27 = table.insertRow();
+var cell27 = row27.insertCell();
+cell27.textContent = "Row 27 text";
+
+// Row 28 (3 columns) - Roboto Light
+var row28 = table.insertRow();
+var cell28_1 = row28.insertCell();
+var cell28_2 = row28.insertCell();
+var cell28_3 = row28.insertCell();
+cell28_1.textContent = "Row 28, Col 1 text";
+cell28_2.textContent = "Row 28, Col 2 text";
+cell28_3.textContent = "Row 28, Col 3 text";
+
+// Row 29
+var row29 = table.insertRow();
+var cell29 = row29.insertCell();
+cell29.textContent = "Row 29 text";
